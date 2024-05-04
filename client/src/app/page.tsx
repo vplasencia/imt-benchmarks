655d92cd53fe70966bd9b5de69ba9026a7bd630d
Vivian Plasencia <v.pcalana@gmail.com>
2024-05-05 01:28:13 +0200

    feat(client): add benchmarks for insert function

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,111 +1,166 @@
-import Image from "next/image"
+"use client"
+
+import dynamic from "next/dynamic"
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false })
+
+import { Bench, Task } from "tinybench"
+
+import { useEffect, useState } from "react"
+
+import { IMT, LeanIMT } from "@zk-kit/imt"
+import { poseidon2 } from "poseidon-lite"
+import { ApexOptions } from "apexcharts"
+
+const tableFunc = (task: Task) => {
+    if (task && task.name && task.result) {
+        return {
+            Function: task.name,
+            "ops/sec": task.result.error ? "NaN" : parseInt(task.result.hz.toString(), 10).toLocaleString(),
+            "Average Time (ms)": task.result.error ? "NaN" : task.result.mean,
+            Margin: task.result.error ? "NaN" : `\xb1${task.result.rme.toFixed(2)}%`,
+            Samples: task.result.error ? "NaN" : task.result.samples.length
+        }
+    }
+}
+
+export type ChartProps = {
+    options: ApexOptions
+    series: ApexAxisChartSeries
+}
 
 export default function Home() {
+    const [insertConfig, setInsertConfig] = useState({
+        options: {
+            chart: {
+                id: "basic-bar"
+            },
+            xaxis: {
+                categories: [1, 2, 3]
+            }
+        },
+        series: [
+            {
+                name: "series-1",
+                data: [1, 2, 3]
+            }
+        ]
+    })
+    // const config = {
+    //     options: {
+    //         chart: {
+    //             id: "basic-bar"
+    //         },
+    //         xaxis: {
+    //             categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+    //         }
+    //     },
+    //     series: [
+    //         {
+    //             name: "series-1",
+    //             data: [30, 40, 45, 50, 49, 60, 70, 91]
+    //         }
+    //     ]
+    // }
+    useEffect(() => {
+        const func = async () => {
+            const bench = new Bench({ time: 0, iterations: 200 })
+            const imtDepth = 16
+            const imtZeroValue = 0
+            const imtArity = 2
+            let imt: IMT
+
+            const leanIMTHash = (a: any, b: any) => poseidon2([a, b])
+            let leanIMT: LeanIMT
+
+            bench
+                .add(
+                    "IMT - Insert",
+                    () => {
+                        imt.insert(1n)
+                    },
+                    {
+                        beforeAll: () => {
+                            imt = new IMT(poseidon2, imtDepth, imtZeroValue, imtArity)
+                        }
+                    }
+                )
+                .add(
+                    "LeanIMT - Insert",
+                    () => {
+                        leanIMT.insert(1n)
+                    },
+                    {
+                        beforeAll: () => {
+                            leanIMT = new LeanIMT(leanIMTHash)
+                        }
+                    }
+                )
+            await bench.warmup()
+            await bench.run()
+
+            // console.log(bench.results[0])
+
+            const config = {
+                options: {
+                    chart: {
+                        id: "basic-bar"
+                    },
+                    xaxis: {
+                        categories: Array.from({ length: 200 }, (_, i) => i + 1),
+                        title: {
+                            text: "Members"
+                        },
+                        labels: {
+                            show: true,
+                            rotate: 0,
+                            rotateAlways: true,
+                            hideOverlappingLabels: true,
+                            trim: false
+                        },
+                        tickAmount: 5
+                    },
+                    yaxis: [
+                        {
+                            title: {
+                                text: "Time (ms)"
+                            },
+                            labels: {
+                                formatter: function (val: number) {
+                                    return val.toFixed(2)
+                                }
+                            }
+                        }
+                    ]
+                },
+                series: [
+                    {
+                        name: "IMT",
+                        data: bench.results[0] ? bench.results[0].samples : []
+                    },
+                    {
+                        name: "LeanIMT",
+                        data: bench.results[1] ? bench.results[1].samples : []
+                    }
+                ]
+            }
+
+            setInsertConfig(config)
+        }
+        func()
+    }, [])
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-                <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-                    Get started by editing&nbsp;
-                    <code className="font-mono font-bold">src/app/page.tsx</code>
-                </p>
-                <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-                    <a
-                        className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-                        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        By{" "}
-                        <Image
-                            src="/vercel.svg"
-                            alt="Vercel Logo"
-                            className="dark:invert"
-                            width={100}
-                            height={24}
-                            priority
-                        />
-                    </a>
+        <div className="app">
+            <div className="row">
+                <div className="mixed-chart">
+                    <Chart
+                        options={insertConfig.options}
+                        series={insertConfig.series}
+                        type="line"
+                        width="800"
+                        height="500"
+                    />
                 </div>
             </div>
-
-            <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-                <Image
-                    className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-                    src="/next.svg"
-                    alt="Next.js Logo"
-                    width={180}
-                    height={37}
-                    priority
-                />
-            </div>
-
-            <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-                <a
-                    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className="mb-3 text-2xl font-semibold">
-                        Docs{" "}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className="m-0 max-w-[30ch] text-sm opacity-50">
-                        Find in-depth information about Next.js features and API.
-                    </p>
-                </a>
-
-                <a
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className="mb-3 text-2xl font-semibold">
-                        Learn{" "}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className="m-0 max-w-[30ch] text-sm opacity-50">
-                        Learn about Next.js in an interactive course with&nbsp;quizzes!
-                    </p>
-                </a>
-
-                <a
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className="mb-3 text-2xl font-semibold">
-                        Templates{" "}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className="m-0 max-w-[30ch] text-sm opacity-50">Explore starter templates for Next.js.</p>
-                </a>
-
-                <a
-                    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <h2 className="mb-3 text-2xl font-semibold">
-                        Deploy{" "}
-                        <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                            -&gt;
-                        </span>
-                    </h2>
-                    <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-                        Instantly deploy your Next.js site to a shareable URL with Vercel.
-                    </p>
-                </a>
-            </div>
-        </main>
+        </div>
     )
 }
